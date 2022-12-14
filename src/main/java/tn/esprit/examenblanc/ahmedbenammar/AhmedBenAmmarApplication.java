7b9615106f9e819538c16bf49edaae72b6affccd
ahmedbnammar <ahmed.benammar1@esprit.tn>
2022-12-14 09:20:22 +0100

    Initial commit

---
+++ src/main/java/tn/esprit/examenblanc/ahmedbenammar/AhmedBenAmmarApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.examenblanc.ahmedbenammar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhmedBenAmmarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AhmedBenAmmarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/esprit/examenblanc/ahmedbenammar/AhmedBenAmmarApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.examenblanc.ahmedbenammar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhmedBenAmmarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
